20f89175b0581892d14ab4f3edecaa1385d1727f
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-07 19:59:36 +0530

    connected mongoDB

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(
+  "process.env.MONGO_URL",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.listen(8800, () => {
+  console.log("Server is running on port 8800");
+});
