098dd533ff5c11583d721b0fa21b35f8d683a4f6
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-10 12:23:44 +0530

    like/dislike and get a post api calls

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -42,8 +42,31 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// like a post
+// like/dislike a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked ");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked ");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // get timeline posts
 module.exports = router;
