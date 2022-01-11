d1513b0359d204a6c6c758bd29da3b6fe29ec27c
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-11 14:58:27 +0530

    Built Sidebar

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Topbar from "../../Topbar/Topbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Topbar from "../../components/Topbar/Topbar";
+import Feed from "../../components/Feed/Feed";
+import Rightbar from "../../components/Rightbar/Rightbar";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Topbar />
-    </div>
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   );
 };
 
