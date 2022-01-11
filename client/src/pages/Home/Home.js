0c06d1c7076bb82c2ec03c05698a5d834df3bd69
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-11 12:04:36 +0530

    Built Topbar

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "../../Topbar/Topbar";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default Home;
