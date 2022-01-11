625dd63f829d4b9f97176d7081e7ebb4e9b0fba7
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-11 11:32:58 +0530

    get timeline posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -69,4 +69,19 @@ router.get("/:id", async (req, res) => {
 });
 
 // get timeline posts
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
