4b3a2bdbbb26ea9c1bf4e0f8426a0ceae0cad0ae
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-12 17:41:01 +0530

    Built Login and Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Profile />
+      <Register />
     </div>
   );
 }
