88fc0b50e2e0a4f16b28950e40718099e819452f
dhwajsharma <dhwajsharma10@gmail.com>
2022-02-05 11:41:08 +0530

    Implemented React Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,17 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import Register from "./pages/Register/Register";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Register />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
